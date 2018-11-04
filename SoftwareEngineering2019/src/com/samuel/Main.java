03c46ed5a62f427cc8618ec574adb701bc7871e2
haveaniceday33 <munrosamuel@gmail.com>
2018-11-03 21:01:41 -0400

    3rd Commit

--- SoftwareEngineering2019/src/com/samuel/Main.java
+++ SoftwareEngineering2019/src/com/samuel/Main.java
@@ -4,11 +4,12 @@ import com.osreboot.ridhvl.display.collection.HvlDisplayModeDefault;
 import com.osreboot.ridhvl.template.HvlTemplateInteg2D;
 
 public class Main extends HvlTemplateInteg2D{
+	
 	public static void main(String [] args){
 		new Main();
 	}
 	public Main(){
-		super(60, 1920, 1080, "Auto Mapping Client v3", new HvlDisplayModeDefault());
+		super(60, 1920, 1080, "Insert Game Title Here", new HvlDisplayModeDefault());
 	}
 	@Override
 	public void initialize() {
