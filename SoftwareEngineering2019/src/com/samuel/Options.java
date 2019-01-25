7f83363bd372d4929c081ea54d8e876fed62db7a
Sam M <munrosamuel@gmail.com>
2019-01-25 09:56:04 -0500

    added comments

--- SoftwareEngineering2019/src/com/samuel/Options.java
+++ SoftwareEngineering2019/src/com/samuel/Options.java
@@ -2,6 +2,17 @@ package com.samuel;
 
 import java.io.Serializable;
 
+/**
+ * @author Samuel Munro
+ * The Options class
+ * 
+ * <p>The Options class declares and defines the defaults for the in-game
+ * options, sound effects and background music. This class is
+ * instantiated if a settings.cfg file is not created. The options are
+ * serialized as well, making for quick reading and easy integration with
+ * RIDHVL</p>
+ */
+
 public class Options implements Serializable{
 	private static final long serialVersionUID = -2245978679489813019L; //AUTO GENERATED
 	
